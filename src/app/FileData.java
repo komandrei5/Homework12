fded1bb6b6f2584b57e12246ca0b7e56ddb96bd0
Andriy Svidlo <komandrei5@gmail.com>
2023-06-01 22:00:08 +0300

    Has edited commit

--- src/app/FileData.java
+++ src/app/FileData.java
@@ -1,4 +1,34 @@
 package app;
 
 public class FileData {
+    private String name;
+    private long size;
+    private String path;
+
+    public FileData(String name, long size, String path) {
+        this.name = name;
+        this.size = size;
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    @Override
+    public String toString() {
+        return "FileData{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                ", path='" + path + '\'' +
+                '}';
+    }
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,43 @@
 package app;
 
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        FileNavigator fileNavigator = new FileNavigator();
+
+        fileNavigator.add( new FileData("file1.txt", 100, "/path/to/file"));
+        fileNavigator.add( new FileData("file2.txt", 200, "/path/to/file"));
+        fileNavigator.add( new FileData("file3.txt", 150, "/another/path"));
+        fileNavigator.add( new FileData("file5.txt", 400, "/another/path"));
+        fileNavigator.add( new FileData("file6.txt", 300, "/another/path"));
+
+        fileNavigator.printCol();
+
+
+        List<FileData> filesAtPath = fileNavigator.find("/path/to/file");
+        System.out.println("Files at /path/to/file: ");
+        for(FileData file : filesAtPath) {
+            System.out.println(file.getName());
+        }
+
+        List<FileData> filteredFiles = fileNavigator.filterBySize( 150);
+        System.out.println("\n Files at /path/to/file with size <= 150:");
+        for (FileData file : filteredFiles) {
+            System.out.println(file.getName());
+        }
+
+        fileNavigator.remove("/path/to/file");
+        List<FileData> removedFiles = fileNavigator.find("/path/to/file");
+        System.out.println("\n Files at /path/to/file after removal:");
+        for (FileData file : removedFiles) {
+            System.out.println(file.getName());
+        }
+        List<FileData> sortedFiles = fileNavigator.sortBySize();
+        System.out.println("\n All files sorted by size: ");
+        for (FileData file : sortedFiles) {
+            System.out.println(file.getName() + " - " + file.getSize());
+        }
+
+    }
 }
